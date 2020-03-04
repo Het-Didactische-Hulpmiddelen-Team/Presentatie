823f1cd59d66aafb6e766b498b16cba130863c5e
BelgianNoise <king.arthur360@gmail.com>
2020-03-04 10:57:10 +0100

    demo flask en jinja

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Welcome Home!"
-
 @app.route("/<text>")
 def displayTest(text):
     return text
+@app.route("/splitter/<text>")
+def splitter(text):
+    return render_template("splitter.html", string=text)
 
 if __name__ == "__main__":
     app.run(port=8080)
